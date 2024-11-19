c262db372fc986535f98aa963bb5437fc97a5490
NicolasH2604 <nicolas.haus@stud.tu-darmstadt.de>
2024-11-19 12:27:24 +0100

    DF-24

--- src/dataland_qa_lab/dataland/get_data.py
+++ src/dataland_qa_lab/dataland/get_data.py
@@ -11,7 +11,7 @@ def _get_data_set_by_year_(company_id: str, year: str) -> str:
     # Eintragen aus welcher Periode man das Dataset haben will -> In diesem Fall 2024
     data_id = "test"
     for t in range(len(dataset)):
-        if (dataset[t].meta_info.reporting_period == year):
+        if dataset[t].meta_info.reporting_period == year:
             data_id = dataset[t].meta_info.data_id
             break
     data = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(data_id=data_id)
