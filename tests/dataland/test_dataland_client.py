70513fc56ff9e497d15e1dcfebe8391602fdd72f
TilmanNiem <117115402+TilmanNiem@users.noreply.github.com>
2024-11-21 18:41:58 +0100

    Df 35 provide llm answer as report (#10)
    * provide basic data extraction class
    
    * DF-24
    
    * DF-24
    
    * fix formatting errors
    
    * fix formatting v2
    
    * fix sonar tests
    
    * sonar fix v2
    
    * sonar v3
    
    * sonar
    
    * sonar vKeineAHnung
    
    * sonar vHoffentlich jetzt
    
    * try mocking
    
    * sonar
    
    * try improve test coverage
    
    * Fix test
    
    ---------
    
    Co-authored-by: NicolasH2604 <nicolas.haus@stud.tu-darmstadt.de>
    Co-authored-by: MarcTM01 <marc@marctm.com>

--- tests/dataland/test_dataland_client.py
+++ tests/dataland/test_dataland_client.py
@@ -1,3 +1,12 @@
+import io
+from typing import Any
+from unittest import mock
+
+from azure.ai.documentintelligence.models import AnalyzeResult
+from openai.types.chat.chat_completion import ChatCompletion, ChatCompletionMessage, Choice
+from pypdf import PdfReader
+
+import dataland_qa_lab.dataland.data_extraction as da
 import dataland_qa_lab.dataland.get_data as qa
 from dataland_qa_lab.utils import config
 
@@ -5,14 +14,61 @@ from dataland_qa_lab.utils import config
 def test_dataland_connectivity() -> None:
     client = config.get_config().dataland_client
     resolved_companies = client.company_api.get_companies(chunk_size=1)
+    assert len(resolved_companies) > 0
 
+
+def test_dummy_get_data() -> None:
     company_id = "4423c691-0436-423f-abcb-0a08127ee848"
     year = "2024"
     qa.get_all_company_datasets(company_id=company_id)
     qa.get_data_id_by_year(company_id=company_id, year=year)
     qa.get_dataset_by_year(company_id=company_id, year=year)
     qa.get_value1_by_year(company_id=company_id, year=year)
-    qa.get_datasource_reference_bytes(company_id=company_id, year=year)
-    test_dataland = client.eu_taxonomy_nuclear_and_gas_api  # noqa: F841
+    test = qa.get_datasource_reference_bytes(company_id=company_id, year=year)
 
-    assert len(resolved_companies) > 0
+    assert test is not None
+
+
+def create_document_intelligence_mock() -> AnalyzeResult:
+    return AnalyzeResult(content="")
+
+
+def build_simple_openai_chat_completion(message: str) -> ChatCompletion:
+    return ChatCompletion(
+        id="test",
+        choices=[
+            Choice(
+                finish_reason="stop",
+                index=0,
+                message=ChatCompletionMessage(
+                    content=message,
+                    role="assistant",
+                ),
+            )
+        ],
+        created=0,
+        model="test",
+        object="chat.completion",
+    )
+
+
+@mock.patch("openai.resources.chat.Completions.create", return_value=build_simple_openai_chat_completion("No"))
+@mock.patch(
+    "dataland_qa_lab.dataland.data_extraction.extract_text_of_pdf", return_value=create_document_intelligence_mock()
+)
+def test_dummy_data_extraction(mock_create: Any, mock_extract_text_of_pdf: Any) -> None:  # noqa: ANN401, ARG001
+    dataland_client = da.get_config().dataland_client
+    dataset_by_year = qa.get_dataset_by_year(company_id="4423c691-0436-423f-abcb-0a08127ee848", year="2024")
+
+    dataset_section426 = dataset_by_year.data.general.general.nuclear_energy_related_activities_section426
+    file_id = dataset_section426.data_source.file_reference
+
+    pdf = dataland_client.documents_api.get_document(file_id)
+    pdf_stream = io.BytesIO(pdf)
+    pdf_reader = PdfReader(pdf_stream)
+
+    data = da.get_relevant_page_of_pdf(int(dataset_section426.data_source.page), pdf_reader)
+    text = da.extract_text_of_pdf(data)
+    result = da.extract_section_426(text)
+
+    assert result == "No"
