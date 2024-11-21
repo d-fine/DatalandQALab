87f0a5e6695e1d215eead6b5c6fdba7bd59e3a57
NicolasH2604 <nicolas.haus@stud.tu-darmstadt.de>
2024-11-21 10:55:53 +0100

    Edit Dummy Test

--- tests/dataland/test_dataland_client.py
+++ tests/dataland/test_dataland_client.py
@@ -1,3 +1,4 @@
+import dataland_qa_lab.dataland.get_data as qa
 from dataland_qa_lab.utils import config
 
 
@@ -5,3 +6,14 @@ def test_dataland_connectivity() -> None:
     client = config.get_config().dataland_client
     resolved_companies = client.company_api.get_companies(chunk_size=1)
     assert len(resolved_companies) > 0
+
+
+def dummy_test() -> None:
+    company_id = "4423c691-0436-423f-abcb-0a08127ee848"
+    year = "2024"
+    qa.get_all_company_datasets(company_id=company_id)
+    qa.get_data_id_by_year(company_id=company_id, year=year)
+    qa.get_dataset_by_year(company_id=company_id, year=year)
+    qa.get_value1_by_year(company_id=company_id, year=year)
+    qa.get_datasource_reference_bytes(company_id=company_id, year=year)
+    assert True
