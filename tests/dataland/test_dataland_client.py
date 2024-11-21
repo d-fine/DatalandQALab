2b10d543f2acae6eb3205fa19f3a11ae68eaf93b
TilmanNiem <tilman.niem@gmail.com>
2024-11-21 13:39:16 +0100

    fix sonar tests

--- tests/dataland/test_dataland_client.py
+++ tests/dataland/test_dataland_client.py
@@ -10,22 +10,7 @@ from dataland_qa_lab.utils import config
 def test_dataland_connectivity() -> None:
     client = config.get_config().dataland_client
     resolved_companies = client.company_api.get_companies(chunk_size=1)
-    assert len(resolved_companies) > 0
 
-
-def test_dummy_get_data() -> None:
-    company_id = "4423c691-0436-423f-abcb-0a08127ee848"
-    year = "2024"
-    qa.get_all_company_datasets(company_id=company_id)
-    qa.get_data_id_by_year(company_id=company_id, year=year)
-    qa.get_dataset_by_year(company_id=company_id, year=year)
-    qa.get_value1_by_year(company_id=company_id, year=year)
-    qa.get_datasource_reference_bytes(company_id=company_id, year=year)
-
-    assert True
-
-
-def test_dummy_data_extraction() -> None:
     dataland_client = da.get_config().dataland_client
 
     dataset_by_year = qa.get_dataset_by_year(company_id="4423c691-0436-423f-abcb-0a08127ee848", year="2024")
@@ -41,5 +26,16 @@ def test_dummy_data_extraction() -> None:
     data = da.get_relevant_page_of_pdf(1, pdf_reader)
     text = da.extraxt_text_of_pdf(data)
     da.extract_section_426(text)
+    assert len(resolved_companies) > 0
+
+
+def test_dummy_get_data() -> None:
+    company_id = "4423c691-0436-423f-abcb-0a08127ee848"
+    year = "2024"
+    qa.get_all_company_datasets(company_id=company_id)
+    qa.get_data_id_by_year(company_id=company_id, year=year)
+    qa.get_dataset_by_year(company_id=company_id, year=year)
+    qa.get_value1_by_year(company_id=company_id, year=year)
+    qa.get_datasource_reference_bytes(company_id=company_id, year=year)
 
     assert True
