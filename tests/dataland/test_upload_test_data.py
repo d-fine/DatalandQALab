7f8fced98998cdbf117525a261d704328f2f9ed1
JonathanFrey2003 <186038973+JonathanFrey2003@users.noreply.github.com>
2024-11-21 12:00:19 +0100

    Revert "Return data ids of test dataset"
    This reverts commit afa79cb8750999976828f166a97220753148221a.

--- tests/dataland/test_upload_test_data.py
+++ tests/dataland/test_upload_test_data.py
@@ -1,11 +1,5 @@
-from pathlib import Path
-
 from src.dataland_qa_lab.dataland.upload_test_data import upload_test_data
 
 
 def test_upload_test_data() -> None:
-    project_root = Path(__file__).resolve().parent.parent.parent
-    pdf_path = project_root / "data" / "pdfs"
-    json_path = project_root / "data" / "jsons"
-    test_data = upload_test_data(pdf_path=pdf_path, json_path=json_path)
-    assert len(test_data) == 10
+    upload_test_data()
