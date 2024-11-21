cce53950bb5f3fca851c31271ae8c65648cda846
NicolasH2604 <nicolas.haus@stud.tu-darmstadt.de>
2024-11-21 10:44:20 +0100

    DF-24

--- src/dataland_qa_lab/dataland/get_data.py
+++ src/dataland_qa_lab/dataland/get_data.py
@@ -6,15 +6,17 @@ conf = config.get_config()
 dataland_client = conf.dataland_client
 
 
-def _get_all_company_datasets_(company_id: str) -> str:
+def get_all_company_datasets(company_id: str) -> str:
+    """Laden aller Datasets aus jeder Periode."""
     api = dataland_client.eu_taxonomy_nuclear_and_gas_api
     dataset = api.get_all_company_nuclear_and_gas_data(company_id=company_id)
 
     return dataset
 
 
-def _get_data_id_by_year_(company_id: str, year: str) -> str:
-    dataset = _get_all_company_datasets_(company_id=company_id)
+def get_data_id_by_year(company_id: str, year: str) -> str:
+    """Data_ID der gewünschten Periode des Datensets erhalten."""
+    dataset = get_all_company_datasets(company_id=company_id)
     # Eintragen aus welcher Periode man das Dataset haben will -> In diesem Fall 2024
     data_id = "test"
     for t in range(len(dataset)):
@@ -25,20 +27,23 @@ def _get_data_id_by_year_(company_id: str, year: str) -> str:
     return data_id
 
 
-def _get_dataset_by_year_(company_id: str, year: str) -> CompanyAssociatedDataNuclearAndGasData:
-    data_id = _get_data_id_by_year_(company_id=company_id, year=year)
+def get_dataset_by_year(company_id: str, year: str) -> CompanyAssociatedDataNuclearAndGasData:
+    """Laden eines bestimmten Datensets einer bestimmten Periode."""
+    data_id = get_data_id_by_year(company_id=company_id, year=year)
     data = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(data_id=data_id)
     return data
 
 
-def _get_value1_by_year_(company_id: str, year: str) -> str:
-    data = _get_dataset_by_year_(company_id=company_id, year=year)
+def get_value1_by_year(company_id: str, year: str) -> str:
+    """Rückgabe des ersten Wertes in dem bestimmten Datenset."""
+    data = get_dataset_by_year(company_id=company_id, year=year)
     value1 = data.data.general.general.nuclear_energy_related_activities_section426.value.value
     return value1
 
 
-def _get_datasource_reference_bytes_(company_id: str, year: str) -> str:
-    data = _get_dataset_by_year_(company_id=company_id, year=year)
+def get_datasource_reference_bytes(company_id: str, year: str) -> str:
+    """Erhalten der Datenreferenz in Bytes, wenn eine Datenquelle hinterlegt ist."""
+    data = get_dataset_by_year(company_id=company_id, year=year)
     value1 = data.data.general.general.nuclear_energy_related_activities_section426
     document_bytes = dataland_client.documents_api.get_document(value1.data_source.file_reference)
     return document_bytes
