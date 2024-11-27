fdf43983cf2b23f57062382b1956b40db4434d69
aardunne <aaron.dunne@gmx.de>
2024-11-27 12:45:11 +0100

    Kommentare raus

--- src/dataland_qa_lab/dataland/error_handling.py
+++ src/dataland_qa_lab/dataland/error_handling.py
@@ -1,19 +1,16 @@
 class DuplicateIDError(Exception):
-    """Exception für doppelte IDs."""
     def __init__(self, data_id) -> None:
         super().__init__(f"Duplicate ID detected: {data_id}")
         self.data_id = data_id
 
 
 class NetworkError(Exception):
-    """Exception für Netzwerkfehler."""
     def __init__(self, error) -> None:
         super().__init__(f"Network error occurred: {error}")
         self.error = error
 
 
 class UnknownError(Exception):
-    """Exception für unbekannte Fehler."""
     def __init__(self, error) -> None:
         super().__init__(f"Unknown error occurred: {error}")
         self.error = error
@@ -21,28 +18,24 @@ class UnknownError(Exception):
 
 class ErrorHandling:
     def __init__(self) -> None:
-        self.duplicate_ids = set()  # Set für doppelte IDs
-        self.network_errors = []  # Liste für Netzwerkfehler
-        self.unknown_errors = []  # Liste für unbekannte Fehler
+        self.duplicate_ids = set()
+        self.network_errors = []
+        self.unknown_errors = []
 
     def log_duplicate_id(self, data_ids, current_data_id) -> any:
-        """Loggt eine doppelte ID."""
         if current_data_id in data_ids:
             self.duplicate_ids.add(current_data_id)
             raise DuplicateIDError(current_data_id)
 
     def log_network_error(self, error) -> any:
-        """Loggt einen Netzwerkfehler."""
         self.network_errors.append(str(error))
         raise NetworkError(error)
 
     def log_unknown_error(self, error) -> any:
-        """Loggt einen unbekannten Fehler."""
         self.unknown_errors.append(str(error))
         raise UnknownError(error)
 
     def report(self) -> dict[str, int]:
-        """Erstellt einen Bericht über alle Fehler."""
         report = {
             "Duplicate IDs": len(self.duplicate_ids),
             "Network Errors": len(self.network_errors),
