a9d83a0604d7035c1ea91c2a50074fde9667357b
JonathanFrey2003 <186038973+JonathanFrey2003@users.noreply.github.com>
2024-12-01 16:07:59 +0100

    Change function calls to use data_id instead of company_id

--- src/dataland_qa_lab/dataland/get_data.py
+++ src/dataland_qa_lab/dataland/get_data.py
@@ -1,32 +1,5 @@
 from dataland_backend.models.company_associated_data_nuclear_and_gas_data import CompanyAssociatedDataNuclearAndGasData
-
-from dataland_qa_lab.dataland.dataland_client import DatalandClient
-
-
-def get_all_company_datasets(company_id: str, dataland_client: DatalandClient) -> str:
-    """Get all datasets of a certain company."""
-    api = dataland_client.eu_taxonomy_nuclear_and_gas_api
-    datasets = api.get_all_company_nuclear_and_gas_data(company_id=company_id)
-
-    return datasets
-
-
-def get_data_id_by_year(company_id: str, year: str, dataland_client: DatalandClient) -> str:
-    """Get Data_ID of given company within wanted period."""
-    dataset = get_all_company_datasets(company_id=company_id, dataland_client=dataland_client)
-    data_id = ""
-    for t in range(len(dataset)):
-        if dataset[t].meta_info.reporting_period == year:
-            data_id = dataset[t].meta_info.data_id
-            break
-
-    return data_id
-
-
-def get_data_by_data_id(data_id: str, dataland_client: DatalandClient) -> CompanyAssociatedDataNuclearAndGasData:
-    """Get certain dataset with data_id."""
-    data = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(data_id=data_id)
-    return data
+from dataland_backend.models.extended_document_reference import ExtendedDocumentReference
 
 
 def get_values_by_data(data: CompanyAssociatedDataNuclearAndGasData) -> list[str]:
@@ -60,32 +33,20 @@ def get_values_by_data(data: CompanyAssociatedDataNuclearAndGasData) -> list[str
 
 
 def get_datasource_reference_bytes(
-    data: CompanyAssociatedDataNuclearAndGasData, dataland_client: DatalandClient
-) -> list[bytearray]:
+    data: CompanyAssociatedDataNuclearAndGasData,
+) -> list[ExtendedDocumentReference | None]:
     """Get list of data references in bytes from given dataset."""
     referenced_reports = data.data.general.general
     document_byte_list = []
 
     document_byte_list.extend(
         (
-            dataland_client.documents_api.get_document(
-                referenced_reports.nuclear_energy_related_activities_section426.data_source.file_reference
-            ),
-            dataland_client.documents_api.get_document(
-                referenced_reports.nuclear_energy_related_activities_section427.data_source.file_reference
-            ),
-            dataland_client.documents_api.get_document(
-                referenced_reports.nuclear_energy_related_activities_section428.data_source.file_reference
-            ),
-            dataland_client.documents_api.get_document(
-                referenced_reports.fossil_gas_related_activities_section429.data_source.file_reference
-            ),
-            dataland_client.documents_api.get_document(
-                referenced_reports.fossil_gas_related_activities_section430.data_source.file_reference
-            ),
-            dataland_client.documents_api.get_document(
-                referenced_reports.fossil_gas_related_activities_section431.data_source.file_reference
-            ),
+            referenced_reports.nuclear_energy_related_activities_section426.data_source,
+            referenced_reports.nuclear_energy_related_activities_section427.data_source,
+            referenced_reports.nuclear_energy_related_activities_section428.data_source,
+            referenced_reports.fossil_gas_related_activities_section429.data_source,
+            referenced_reports.fossil_gas_related_activities_section430.data_source,
+            referenced_reports.fossil_gas_related_activities_section431.data_source,
         )
     )
     return document_byte_list
--- tests/dataland/test_dataland_client.py
+++ tests/dataland/test_dataland_client.py
@@ -8,6 +8,7 @@ from pypdf import PdfReader
 
 import dataland_qa_lab.dataland.data_extraction as da
 import dataland_qa_lab.dataland.get_data as qa
+import dataland_qa_lab.dataland.provide_test_data
 from dataland_qa_lab.utils import config
 
 
@@ -18,15 +19,20 @@ def test_dataland_connectivity() -> None:
 
 
 def test_dummy_get_data() -> None:
-    company_id = "4423c691-0436-423f-abcb-0a08127ee848"
-    year = "2024"
-    qa.get_all_company_datasets(company_id=company_id)
-    qa.get_data_id_by_year(company_id=company_id, year=year)
-    qa.get_dataset_by_year(company_id=company_id, year=year)
-    qa.get_value1_by_year(company_id=company_id, year=year)
-    test = qa.get_datasource_reference_bytes(company_id=company_id, year=year)
+    dataland_client = config.get_config().dataland_client
+    company_name = "rwe"
+    company_id = dataland_qa_lab.dataland.provide_test_data.get_company_id(
+        company=company_name, dataland_client=dataland_client
+    )
+    data_id = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_all_company_nuclear_and_gas_data(
+        company_id=company_id
+    )[0].meta_info.data_id
+    data = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(data_id=data_id)
+    current_values = qa.get_values_by_data(data=data)
+    test = qa.get_datasource_reference_bytes(data=data)
 
     assert test is not None
+    assert len(current_values) == 6
 
 
 def create_document_intelligence_mock() -> AnalyzeResult:
@@ -58,7 +64,17 @@ def build_simple_openai_chat_completion(message: str) -> ChatCompletion:
 )
 def test_dummy_data_extraction(mock_create: Any, mock_extract_text_of_pdf: Any) -> None:  # noqa: ANN401, ARG001
     dataland_client = da.get_config().dataland_client
-    dataset_by_year = qa.get_dataset_by_year(company_id="4423c691-0436-423f-abcb-0a08127ee848", year="2024")
+    company_name = "rwe"
+    company_id = dataland_qa_lab.dataland.provide_test_data.get_company_id(
+        company=company_name, dataland_client=dataland_client
+    )
+    data_id = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_all_company_nuclear_and_gas_data(
+        company_id=company_id
+    )[0].meta_info.data_id
+
+    dataset_by_year = dataland_client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(
+        data_id=data_id
+    )
 
     dataset_section426 = dataset_by_year.data.general.general.nuclear_energy_related_activities_section426
     file_id = dataset_section426.data_source.file_reference
