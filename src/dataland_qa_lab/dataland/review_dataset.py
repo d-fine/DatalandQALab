fd969927a988b1a7dbef38db41f14a7bf29dfb64
aardunne <aaron.dunne@gmx.de>
2024-12-02 15:51:47 +0100

    Initial Push für Merge

---
+++ src/dataland_qa_lab/dataland/review_dataset.py
@@ -0,0 +1,15 @@
+class DatasetReviewer:
+
+    def __init__(self):
+        """
+        Initialize any necessary resources or configurations for the dataset reviewer.
+        """
+        # Example: Set up logging, database connections, etc.
+        pass
+
+    def review_dataset(self, data_id):
+        """Hello"""
+
+        # data_id -> Section426 -> PDF -> FindPageNumber/PDFtoText -> AllValues426 -> Vergleich
+        # send_report_to_dataland (data_id)
+        # erstellt
