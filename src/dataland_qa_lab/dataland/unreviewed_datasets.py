5200b120feeec1a27af891947bfc6ec9af5dfacf
Si Thu <116348957+Si2-Aung@users.noreply.github.com>
2024-12-03 16:38:45 +0100

    Df 18 into Df 62 (#15)
    * Tests added for unreviewed_dataset class
    
    * Ci Errors fixed
    
    * Ci Errors fixed for real
    
    * Final Ci errors fixes
    
    * inifinite while loop fix
    
    * add test for scheduled_processor
    
    * small test fixes
    
    * feedback implemented
    
    * fix test
    
    ---------
    
    Co-authored-by: aarondunne <117835979+aardunne@users.noreply.github.com>

--- src/dataland_qa_lab/dataland/unreviewed_datasets.py
+++ src/dataland_qa_lab/dataland/unreviewed_datasets.py
@@ -1,22 +1,28 @@
+import logging
+
 from dataland_qa_lab.utils import config
 
+
 class UnreviewedDatasets:
-    """Class to manage unreviewed datasets from the API."""
+    """Class representing the unreviewed datasets from the API."""
 
-    conf = config.get_config()
-    dataland_client = conf.dataland_client
+    def __init__(self) -> None:
+        """Initialize the unreviewed datasets with the data from the API."""
+        self.conf = config.get_config()
+        self.dataland_client = self.conf.dataland_client
 
-    def __init__(self):
         try:
-            amount_of_datasets = self.dataland_client.qa_api.get_number_of_pending_datasets()
-            if amount_of_datasets is None or amount_of_datasets < 0:
-                raise ValueError("Received an invalid number of pending datasets.")
+            number_of_datasets = self.dataland_client.qa_api.get_number_of_pending_datasets()
+            if number_of_datasets is None or number_of_datasets < 0:
+                msg = "Received an invalid number of pending datasets."
+                raise ValueError(msg)  # noqa: TRY301
 
             self.datasets = self.dataland_client.qa_api.get_info_on_pending_datasets(
-                data_types=["nuclear-and-gas"], chunk_size=amount_of_datasets
+                data_types=["nuclear-and-gas"], chunk_size=number_of_datasets
             )
+
             self.list_of_data_ids = [dataset.data_id for dataset in self.datasets]
 
-        except Exception as e:
-            print(f"Error fetching datasets: {e}")
-            raise  # Propagate the error
+        except Exception:
+            logging.exception("An error occurred")
+            raise
