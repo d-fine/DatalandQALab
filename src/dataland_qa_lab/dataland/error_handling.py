002f31185e3eb4b6be7c27cb70d316bed4f5adc7
TilmanNiem <tilman.niem@gmail.com>
2024-12-03 19:39:45 +0100

    solve linting errors

--- src/dataland_qa_lab/dataland/error_handling.py
+++ src/dataland_qa_lab/dataland/error_handling.py
@@ -1,24 +1,32 @@
 class NetworkError(Exception):
-    def init(self, error) -> None:
+    """Log network errors."""
+    def init(self, error: any) -> None:
+        """Constructor."""
         super().init(f"Network error occurred: {error}")
         self.error = error
 
 
 class UnknownError(Exception):
-    def init(self, error) -> None:
+    """log unkwnown errors."""
+    def init(self, error: any) -> None:
+        """Constructor."""
         super().init(f"Unknown error occurred: {error}")
         self.error = error
 
 
 class ErrorHandling:
+    """Error handling class."""
     def init(self) -> None:
+        """"Constructor."""
         self.network_errors = []
         self.unknown_errors = []
 
-    def log_network_error(self, error) -> None:
+    def log_network_error(self, error: any) -> None:
+        """"Network error."""
         self.network_errors.append(str(error))
         raise NetworkError(error)
 
-    def log_unknown_error(self, error) -> None:
+    def log_unknown_error(self, error: any) -> None:
+        """"Unknown error."""
         self.unknown_errors.append(str(error))
         raise UnknownError(error)
--- src/dataland_qa_lab/dataland/review_dataset.py
+++ src/dataland_qa_lab/dataland/review_dataset.py
@@ -1,14 +1,12 @@
 class DatasetReviewer:
-    def __init__(self):
-        """
-        Initialize any necessary resources or configurations for the dataset reviewer.
-        """
+    """Not implemented yet."""
+
+    def __init__(self) -> None:
+        """Initialize any necessary resources or configurations for the dataset reviewer."""
         # Example: Set up logging, database connections, etc.
-        pass
 
-    def review_dataset(self, data_id):
-        """Hello"""
-
+    def review_dataset(self, data_id):  # noqa: ANN001, ANN201
+        """Hello."""
         # data_id -> Section426 -> PDF -> FindPageNumber/PDFtoText -> AllValues426 -> Vergleich
         # send_report_to_dataland (data_id)
         # erstellt
