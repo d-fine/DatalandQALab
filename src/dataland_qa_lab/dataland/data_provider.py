069ab4b549a92e7845748a793720c2f42e8ca0b7
TilmanNiem <tilman.niem@gmail.com>
2024-12-05 12:32:04 +0100

    build basic comparison logic

---
+++ src/dataland_qa_lab/dataland/data_provider.py
@@ -0,0 +1,55 @@
+from dataland_backend.models.extended_document_reference import ExtendedDocumentReference
+from dataland_qa.models.nuclear_and_gas_data import NuclearAndGasData
+
+
+class DataProvider:
+    """Provide certain data from Nuclear and Gas datasets."""
+    @classmethod
+    def get_values_by_data(cls, data: NuclearAndGasData) -> list[str]:
+        """Get Yes/No values of given dataset."""
+        referenced_reports = data.general.general
+        document_value_list = []
+
+        document_value_list.extend(
+            (
+                referenced_reports.nuclear_energy_related_activities_section426.value.value
+                if referenced_reports.nuclear_energy_related_activities_section426.value is not None
+                else None,
+                referenced_reports.nuclear_energy_related_activities_section427.value.value
+                if referenced_reports.nuclear_energy_related_activities_section427.value is not None
+                else None,
+                referenced_reports.nuclear_energy_related_activities_section428.value.value
+                if referenced_reports.nuclear_energy_related_activities_section428.value is not None
+                else None,
+                referenced_reports.fossil_gas_related_activities_section429.value.value
+                if referenced_reports.fossil_gas_related_activities_section429.value is not None
+                else None,
+                referenced_reports.fossil_gas_related_activities_section430.value.value
+                if referenced_reports.fossil_gas_related_activities_section430.value is not None
+                else None,
+                referenced_reports.fossil_gas_related_activities_section431.value.value
+                if referenced_reports.fossil_gas_related_activities_section431.value is not None
+                else None,
+            )
+        )
+        return document_value_list
+
+    @classmethod
+    def get_datasources_of_data_points(cls,
+        data: NuclearAndGasData,
+    ) -> list[ExtendedDocumentReference | None]:
+        """Get list of data references in bytes from given dataset."""
+        referenced_reports = data.general.general
+        document_byte_list = []
+
+        document_byte_list.extend(
+            (
+                referenced_reports.nuclear_energy_related_activities_section426.data_source,
+                referenced_reports.nuclear_energy_related_activities_section427.data_source,
+                referenced_reports.nuclear_energy_related_activities_section428.data_source,
+                referenced_reports.fossil_gas_related_activities_section429.data_source,
+                referenced_reports.fossil_gas_related_activities_section430.data_source,
+                referenced_reports.fossil_gas_related_activities_section431.data_source,
+            )
+        )
+        return document_byte_list
--- src/dataland_qa_lab/review/report_generator.py
+++ src/dataland_qa_lab/review/report_generator.py
@@ -20,6 +20,7 @@ from dataland_qa.models.qa_report_data_point_extended_data_point_yes_no import (
 )
 from dataland_qa.models.qa_report_data_point_verdict import QaReportDataPointVerdict
 
+from dataland_qa_lab.dataland.data_provider import DataProvider
 from dataland_qa_lab.review.yes_no_value_generator import YesNoValueGenerator
 
 
@@ -29,6 +30,7 @@ class ReportGenerator:
     def generate_report(self, relevant_pages: AnalyzeResult, dataset: NuclearAndGasData) -> None:
         """_summary."""
         self.relevant_pages = relevant_pages
+        self.dataset = dataset
 
         self.report = self.build_report_frame()
 
@@ -54,11 +56,34 @@ class ReportGenerator:
     def compare_yes_no_values(self) -> None:
         """Build first yes no data point."""
         yes_no_values = YesNoValueGenerator().extract_section_426(relevant_document=self.relevant_pages)
+        yes_no_values_from_dataland = DataProvider().get_values_by_data(data=self.dataset)
+        data_sources = DataProvider().get_datasources_of_data_points(data=self.dataset)
 
         qa_data_points = []
-        qa_data_points.extend(QaReportDataPointExtendedDataPointYesNo(
-            comment="QA",
-            verdict=QaReportDataPointVerdict.QAACCEPTED,
-            correctedData=ExtendedDataPointYesNo(value=yes_no_values)))
+
+        for i in range(5):
+            corrected_value = yes_no_values[i]
+            dataland_value = yes_no_values_from_dataland[i]
+
+            if corrected_value != dataland_value:
+                qa_data_points[i] = QaReportDataPointExtendedDataPointYesNo(
+                    comment="tbd",
+                    verdict=QaReportDataPointVerdict.QAACCEPTED,
+                    correctedData=ExtendedDataPointYesNo(
+                        value=corrected_value,
+                        quality="?",
+                        comment="justification",
+                        dataSource=data_sources[i])
+                )
+            else:
+                qa_data_points[i] = QaReportDataPointExtendedDataPointYesNo(
+                comment="tbd",
+                verdict=QaReportDataPointVerdict.QAREJECTED
+                )
 
         self.report.general.general.nuclear_energy_related_activities_section426 = qa_data_points[0]
+        self.report.general.general.nuclear_energy_related_activities_section427 = qa_data_points[1]
+        self.report.general.general.nuclear_energy_related_activities_section428 = qa_data_points[2]
+        self.report.general.general.fossil_gas_related_activities_section429 = qa_data_points[3]
+        self.report.general.general.fossil_gas_related_activities_section430 = qa_data_points[4]
+        self.report.general.general.fossil_gas_related_activities_section431 = qa_data_points[5]
