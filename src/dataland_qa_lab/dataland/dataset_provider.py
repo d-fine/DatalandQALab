4a0488f91527b5694b2a7077c979c213b6c71677
TilmanNiem <tilman.niem@gmail.com>
2024-12-09 20:30:58 +0100

    format

--- src/dataland_qa_lab/dataland/dataset_provider.py
+++ src/dataland_qa_lab/dataland/dataset_provider.py
@@ -11,8 +11,6 @@ class DatasetProvider:
     def get_dataset_by_id(cls, data_id: StrictStr) -> CompanyAssociatedDataNuclearAndGasData:
         """Return the nuclear and gas dataset based on the data id."""
         client = config.get_config().dataland_client
-        dataset = client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(
-            data_id=data_id
-        )
+        dataset = client.eu_taxonomy_nuclear_and_gas_api.get_company_associated_nuclear_and_gas_data(data_id=data_id)
 
         return dataset
