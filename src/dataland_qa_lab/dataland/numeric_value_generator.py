8c379a1fd1b03a21bc4b75c1225ec1334c1eb999
chrisBer67 <chrbertleff@gmail.com>
2024-12-10 19:54:08 +0100

    NumericValueGenerator

---
+++ src/dataland_qa_lab/dataland/numeric_value_generator.py
@@ -0,0 +1,28 @@
+# from azure.ai.documentintelligence.models import AnalyzeResult, ContentFormat
+
+class NumericValueGenerator:
+    """Class for generating a numeric value generator."""
+    def __init__(self) -> None:
+        """Initizialize the given generator."""
+
+    def build_prompt(self) -> None:
+        """The specific prompt for the templates 2-5."""
+
+    def get_taxonomy_aligned_denominator() -> None:
+        """Returns the denominator."""
+        # Die Rückgabe ist noch nicht ganz richtig.
+
+    def get_taxonomy_aligned_numerator() -> None:
+        """Returns the numerator."""
+        # Die Rückgabe ist noch nicht ganz richtig.
+
+    def get_taxonomy_eligbile_not_aligned() -> None:
+        """Returns the eligible factor."""
+        # Die Rückgabe ist noch nicht ganz richtig.
+
+    def get_taxonomy_non_eligible_() -> None:
+        """Returns the non eligible factor."""
+        # Die Rückgabe ist noch nicht ganz richtig.
+
+    def compare_values() -> bool:
+        """Compares the given values."""
