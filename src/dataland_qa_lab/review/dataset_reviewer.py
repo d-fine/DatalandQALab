38525170ed597a622c3b6889581edb25acf3c09c
TilmanNiem <tilman.niem@gmail.com>
2024-12-10 20:14:35 +0100

    update import

--- src/dataland_qa_lab/review/dataset_reviewer.py
+++ src/dataland_qa_lab/review/dataset_reviewer.py
@@ -1,7 +1,6 @@
 from dataland_qa_lab.dataland import dataset_provider
-from dataland_qa_lab.pages import pages_provider
-from dataland_qa_lab.pages.text_to_doc_intelligence import TextToDocIntelligence
-from dataland_qa_lab.review.report_generator.nuclear_and_gas_report_generator import ReportGenerator
+from dataland_qa_lab.pages import pages_provider, text_to_doc_intelligence
+from dataland_qa_lab.review.report_generator.nuclear_and_gas_report_generator import NuclearAndGasReportGenerator
 from dataland_qa_lab.utils import config
 
 
@@ -11,9 +10,9 @@ def review_dataset(data_id: str) -> str | None:
 
     relevant_pages_pdf_reader = pages_provider.get_relevant_pages_of_pdf(dataset.data)
 
-    readable_text = TextToDocIntelligence().extract_text_of_pdf(relevant_pages_pdf_reader)
+    readable_text = text_to_doc_intelligence.extract_text_of_pdf(relevant_pages_pdf_reader)
 
-    report = ReportGenerator().generate_report(relevant_pages=readable_text, dataset=dataset.data)
+    report = NuclearAndGasReportGenerator().generate_report(relevant_pages=readable_text, dataset=dataset.data)
 
     config.get_config().dataland_client.eu_taxonomy_nuclear_gas_qa_api.post_nuclear_and_gas_data_qa_report(
         data_id=data_id, nuclear_and_gas_data=report
