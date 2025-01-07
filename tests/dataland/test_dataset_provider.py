80c0090235c0494d6508e7df67a4e98653d38e0a
jonkra20 <jonas.kraemer@stud.tu-darmstadt.de>
2025-01-07 13:48:08 +0100

    Test-Anpassung 3

--- tests/dataland/test_dataset_provider.py
+++ tests/dataland/test_dataset_provider.py
@@ -2,7 +2,7 @@ from dataland_qa_lab.dataland import dataset_provider
 
 
 def test_get_dataset() -> None:
-    test_data_id = "9ae2300a-db98-4815-abee-152a24cd3039"
+    test_data_id = "7b7c7ea2-7d74-4161-afc8-4aa6bcde66c7"
     dataset = dataset_provider.get_dataset_by_id(data_id=test_data_id)
 
     assert dataset.reporting_period is not None
