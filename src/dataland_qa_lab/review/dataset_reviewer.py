f679c6ee02eb9eba60d2a06f9a92eab4229a87e5
TilmanNiem <117115402+TilmanNiem@users.noreply.github.com>
2025-01-28 10:27:10 +0100

    Df 127 end to end test (#36)
    * add base for end to end test and basic logic for mocking azure with different prompts
    
    * create mock_constants, they still throw an error
    
    * fix string
    
    * Upload test dataset with deliberate mistakes and save data id
    
    * test correctness of qa report
    
    * Fix missing parameter of function call
    
    ---------
    
    Co-authored-by: JonathanFrey2003 <186038973+JonathanFrey2003@users.noreply.github.com>

--- src/dataland_qa_lab/review/dataset_reviewer.py
+++ src/dataland_qa_lab/review/dataset_reviewer.py
@@ -1,3 +1,5 @@
+from dataland_qa.models.qa_report_meta_information import QaReportMetaInformation
+
 from dataland_qa_lab.dataland import dataset_provider
 from dataland_qa_lab.pages import pages_provider, text_to_doc_intelligence
 from dataland_qa_lab.review.report_generator.nuclear_and_gas_report_generator import NuclearAndGasReportGenerator
@@ -5,7 +7,7 @@ from dataland_qa_lab.utils import config
 from dataland_qa_lab.utils.nuclear_and_gas_data_collection import NuclearAndGasDataCollection
 
 
-def review_dataset(data_id: str) -> str | None:
+def review_dataset(data_id: str) -> QaReportMetaInformation | None:
     """Review a dataset."""
     dataset = dataset_provider.get_dataset_by_id(data_id)
 
@@ -21,6 +23,10 @@ def review_dataset(data_id: str) -> str | None:
 
     report = NuclearAndGasReportGenerator().generate_report(relevant_pages=readable_text, dataset=data_collection)
 
-    config.get_config().dataland_client.eu_taxonomy_nuclear_gas_qa_api.post_nuclear_and_gas_data_qa_report(
-        data_id=data_id, nuclear_and_gas_data=report
+    report_data = (
+        config.get_config().dataland_client.eu_taxonomy_nuclear_gas_qa_api.post_nuclear_and_gas_data_qa_report(
+            data_id=data_id, nuclear_and_gas_data=report
+        )
     )
+
+    return report_data
