a990521e76c517fe24f315a4c4f3effb33eb1657
TilmanNiem <tilman.niem@gmail.com>
2025-02-13 14:58:59 +0100

    try patch v3

--- tests/end_to_end/test_report_e2e.py
+++ tests/end_to_end/test_report_e2e.py
@@ -13,13 +13,15 @@ from dataland_qa_lab.review.dataset_reviewer import review_dataset
 from dataland_qa_lab.utils import config
 
 
-def test_report_generator_end_to_end() -> None:
+@patch("dataland_qa_lab.dataland.alerting.send_alert_message")
+def test_report_generator_end_to_end(mock_alert_system: MagicMock,) -> None:
     """
     This test is supposed to test the entire process of generating a Quality-Assurance-Report for a
     EU Taxonomy Nuclear and Gas dataset from dataland.com.
     Only the communication to Azure is mocked, all other components and their interactions
     should be tested during this test.
     """
+    mock_alert_system.return_value = None
 
     # Upload test_dataset with partly wrong data
     data_id = upload_test_dataset()
@@ -89,17 +91,14 @@ def test_report_generator_end_to_end() -> None:
 
 @patch("dataland_qa_lab.pages.text_to_doc_intelligence.extract_text_of_pdf")
 @patch("dataland_qa_lab.database.database_engine.get_entity")
-@patch("dataland_qa_lab.dataland.alerting.send_alert_message")
 def mocked_review_dataset(
     data_id: str,
-    mock_alert_system: MagicMock,
     mock_get_entity: MagicMock,
     mock_extract_text_of_pdf: MagicMock,
 ) -> QaReportMetaInformation:
     """Review the dataset with mocked Azure calls."""
     mock_extract_text_of_pdf.return_value = mock_constants.E2E_AZURE_DOCUMENT_INTELLIGENCE_MOCK
     mock_get_entity.return_value = None
-    mock_alert_system.return_value = None
     with patch("openai.resources.chat.Completions.create", side_effect=mock_open_ai):
         report_data = review_dataset(data_id=data_id, single_pass_e2e=True)
         return report_data
