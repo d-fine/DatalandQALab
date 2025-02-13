924f3f64bd49e45e75bc3872c5d5a1f84686fa19
TilmanNiem <tilman.niem@gmail.com>
2025-02-13 20:46:26 +0100

    fix try startup

--- src/dataland_qa_lab/bin/server.py
+++ src/dataland_qa_lab/bin/server.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 from fastapi import FastAPI
+from starlette.background import BackgroundTasks
 
 from dataland_qa_lab.database.database_engine import create_tables
 from dataland_qa_lab.dataland import scheduled_processor
@@ -14,7 +15,6 @@ logger = logging.getLogger("dataland_qa_lab.bin.server")
 def main(single_pass_e2e: bool = False) -> None:
     """Launch the QA Lab server."""
     logger.info("Launching the Dataland QA Lab server")
-    dataland_qa_lab = FastAPI(title="FastAPI")
     console_logger.configure_console_logger()
     create_tables()
 
@@ -27,7 +27,15 @@ def main(single_pass_e2e: bool = False) -> None:
         time.sleep(30)
 
 
-dataland_qa_lab = main()
+dataland_qa_lab = FastAPI(title="FastAPI")
+
+
+@dataland_qa_lab.on_event("startup")
+async def startup_event() -> None:
+    """Ensure start of main after startup."""
+    background_tasks = BackgroundTasks()
+    background_tasks.add_task(main)
+    await background_tasks()
 
 @dataland_qa_lab.get("/review/{data_id}")
 def review_dataset_endpoint(data_id: str, force_review: bool = False) -> str:
