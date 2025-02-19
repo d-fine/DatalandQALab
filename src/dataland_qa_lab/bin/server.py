8379ee8c5e1810c0df8ec2f5387f6f137ce1c04c
TilmanNiem <tilman.niem@gmail.com>
2025-02-19 23:47:56 +0100

    fix: fix date string

--- src/dataland_qa_lab/bin/server.py
+++ src/dataland_qa_lab/bin/server.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import logging
 from contextlib import asynccontextmanager
 
@@ -12,18 +13,19 @@ from dataland_qa_lab.utils import console_logger
 
 logger = logging.getLogger("dataland_qa_lab.bin.server")
 
+console_logger.configure_console_logger()
+logger.info("Launching the Dataland QA Lab server")
+create_tables()
+
 scheduler = BackgroundScheduler()
-trigger = CronTrigger(minute="*/10", start_date="in 5 Minutes")
+start_time = datetime.now() + timedelta(minutes=1)
+trigger = CronTrigger(minute="*/10", start_date=start_time)
 scheduler.add_job(scheduled_processor.run_scheduled_processing(), trigger)
 scheduler.start()
-
 
 @asynccontextmanager
 async def lifespan(dataland_qa_lab: FastAPI):
     """FastAPI starts first, then runs main()."""
-    console_logger.configure_console_logger()
-    logger.info("Launching the Dataland QA Lab server")
-    create_tables()
     yield
     scheduler.shutdown()
 
