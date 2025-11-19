9cb67bb245dd433c36239479c3a4571090cd84ef
Lena <lena.vo@stud.tu-darmstadt.de>
2025-11-19 12:33:36 +0000

    promprint service wip

--- src/dataland_qa_lab/categories/nuclear_and_gas_data/prompting.py
+++ src/dataland_qa_lab/categories/nuclear_and_gas_data/prompting.py
@@ -1,5 +1,91 @@
+import json
+
+
 class NuclearAndGasPrompting:
     prompt_xyz = ""
 
     def __init__(self) -> None:
         pass
+
+    def generate_prompt_case_1(pdf: str) -> str:
+        json_subprompt = {
+            "type": "object",
+            "properties": {
+                "1": {
+                    "type": "string",
+                    "description": """The precise answer to the first question
+                        of Nuclear energy related activities. "The undertaking carries out,
+                        funds or has exposures to research, development, demonstration and
+                        deployment of innovative electricity generation facilities that
+                        produce energy from nuclear processes with minimal waste from
+                        the fuel cycle." Write only Yes or No.
+                        You need to answer this question.""",
+                },
+                "2": {
+                    "type": "string",
+                    "description": """The precise answer to the second question
+                        of Nuclear energy related activities. "The undertaking carries out,
+                        funds or has exposures to construction and safe operation of new
+                        nuclear installations to produce electricity or process heat,
+                        including for the purposes of district heating or industrial
+                        processes such as hydrogen production, as well as their safety
+                        upgrades, using best available technologies." Write only Yes or No
+                        You need to answer this question.""",
+                },
+                "3": {
+                    "type": "string",
+                    "description": """The precise answer to the third question
+                        of Nuclear energy related activities. "The undertaking carries out,
+                        funds or has exposures to safe operation of existing nuclear
+                        installations that produce electricity or process heat, including
+                        for the purposes of district heating or industrial processes such
+                        as hydrogen production from nuclear energy, as well as their safety
+                        upgrades." Write only Yes or No
+                        You need to answer this question.""",
+                },
+                "4": {
+                    "type": "string",
+                    "description": """The precise answer to question
+                        of Fossil gas related activities. "The undertaking carries out,
+                        funds or has exposures to construction or operation of
+                        electricity generation facilities that produce electricity
+                        using fossil gaseous fuels." Write only Yes or No
+                        You need to answer this question.""",
+                },
+                "5": {
+                    "type": "string",
+                    "description": """The precise answer to the second or fifth question
+                        of Fossil gas related activities. "The undertaking carries out,
+                        funds or has exposures to construction, refurbishment, and
+                        operation of combined heat/cool and power generation facilities
+                        using fossil gaseous fuels." Write only Yes or No
+                        You need to answer this question.""",
+                },
+                "6": {
+                    "type": "string",
+                    "description": """The precise answer to the third or sixth question
+                        of Fossil gas related activities. "The undertaking carries out,
+                        funds or has exposures to construction, refurbishment and
+                        operation of heat generation facilities that produce heat/cool
+                        using fossil gaseous fuels." Write only Yes or No
+                        You need to answer this question.""",
+                },
+            },
+            "required": [
+                "answer_value_1",
+                "answer_value_2",
+                "answer_value_3",
+                "answer_value_4",
+                "answer_value_5",
+                "answer_value_6",
+            ],
+        }
+
+        return f"""Given the information from the [relevant documents],
+        provide the answers of all 6 questions in template 1.
+        Only answer with 'Yes' or 'No'. You need to provide 6 answers.
+        # Relevant Documents
+        {pdf}
+
+        {json.dumps(json_subprompt, indent=4)}
+        """
---
+++ src/dataland_qa_lab/categories/sfdr/prompting.py
@@ -0,0 +1,5 @@
+class SFDRPrompting:
+    prompt_xyz = ""
+
+    def __init__(self) -> None:
+        pass
