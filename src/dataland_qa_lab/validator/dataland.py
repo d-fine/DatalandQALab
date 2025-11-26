6c209ec753d249cd50c398417ed659fac36ff38f
Lena Vo <vo_lena@web.de>
2025-11-26 11:25:24 +0000

    wip

--- src/dataland_qa_lab/validator/dataland.py
+++ src/dataland_qa_lab/validator/dataland.py
@@ -30,5 +30,21 @@ def get_document(document_id: str):
     original_pdf = pypdf.PdfReader(full_pdf_stream)
     output_pdf = pypdf.PdfWriter()
 
-def post_data_point_qwa_report(data_point_id:str, qa_report:dict):
-    # todo: implement
+def post_data_point_qa_report(data_point_id:str, qa_report:dict) -> dict | None:
+    """Post data point QA report to Dataland."""
+    url = f"{conf.dataland_url}/api/data-points/{data_point_id}/qa-reports"
+    
+    res = requests.request(
+        "POST",
+        url,
+        headers=headers,
+        json=qa_report,
+    )
+    
+    #res.ok würde 201, 202 etc. auch abdecken
+    if res.status_code == requests.codes.ok:
+        return res.json()
+    
+    print(f"Error sending report: {res.status_code} - {res.text}")
+    return None
+    
