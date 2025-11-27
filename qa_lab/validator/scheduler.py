bf1c91a84c8adf11971b47f999229ae053473c63
JoshuaKoeck <joshua.koeck@stud.tu-darmstadt.de>
2025-11-27 19:48:13 +0000

    slack integration

--- qa_lab/validator/scheduler.py
+++ qa_lab/validator/scheduler.py
@@ -15,27 +15,31 @@ def run_scheduled_processing() -> None:
     logger.info("Scheduled processing started.")
     unreviewed_datasets = get_pending_datasets()
 
+    send_slack_message("------------------------------------------------------------------")
     logger.info("Found %d unreviewed datasets. Starting processing.", len(unreviewed_datasets))
     for dataset in unreviewed_datasets:
+        send_slack_message(f"⏳ Starting validation for dataset ID: {dataset.get('dataId', '')}")
         logger.info("Processing dataset ID: %s", dataset.get("dataId", ""))
         data_points = get_dataset_data_points(dataset_id=dataset.get("dataId", ""))
 
         qa_status = Counter()
 
         for k, v in data_points.items():
-            send_slack_message(f"⏳ Starting validation for data point ID: {k} of kind {v}")
+            send_slack_message(f"--- ⏳ Starting validation for data point ID: {v} of kind {k}")
             logger.info("Validating of type %s data point ID: %s", k, v)
             try:
                 res = validate_datapoint(v, ai_model=conf.ai_model, use_ocr=conf.use_ocr, override=True)
                 if res.get("qa_status") is QaStatus.Accepted:
-                    send_slack_message(f"✅ Data point ID: {v} accepted.")
+                    send_slack_message(f"--- ✅ Data point ID: {v} accepted.")
                     logger.info("Data point ID: %s accepted.", v)
                 else:
-                    send_slack_message(f"❌ Data point ID: {v} rejected.")
+                    send_slack_message(f"--- ❌ Data point ID: {v} rejected.")
                     logger.info("Data point ID: %s rejected.", v)
                 qa_status[res["qa_status"]] += 1
-            except Exception:
-                send_slack_message(f"🟡 Error processing data point ID: {v}. Report stays in pending.")
+            except Exception as e:
+                send_slack_message(
+                    f"--- 🟡 Error processing data point ID: {v}. Report stays in pending. With the following Error: {e!s}"  # noqa: E501
+                )
                 logger.exception("Error processing data point ID %s", v)
                 qa_status[QaStatus.Pending] += 1
 
