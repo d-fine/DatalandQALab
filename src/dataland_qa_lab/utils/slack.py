7d2686f7b53ffe6c627b2d5b05b2c419244efd24
JoshuaKoeck <joshua.koeck@stud.tu-darmstadt.de>
2025-12-02 00:28:33 +0000

    wip tests (2/?)

--- src/dataland_qa_lab/utils/slack.py
+++ src/dataland_qa_lab/utils/slack.py
@@ -7,15 +7,15 @@ from dataland_qa_lab.utils import config
 
 def send_slack_message(message: str) -> requests.Response | None:
     """Sends an Alert Message to the Slackbot."""
-    url: str | None = config.get_config().slack_webhook_url
-    environment: str | None = config.get_config().environment
+    url = config.get_config().slack_webhook_url
+    environment = config.get_config().environment
+
     if url is None:
         return None
 
-    msg = message if environment is None else "[" + environment + "] " + message
+    msg = "[" + environment + "] " + message if environment else message
 
     payload = {"text": msg}
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(url=url, data=json.dumps(payload), headers=headers)
+    response = requests.post(url=url, json=payload)
 
     return response
