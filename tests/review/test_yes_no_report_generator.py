cd9f031a66ede6e3a389584e3874d9152e9ebf6e
Muriel926 <muriel.schindler@stud.tu-darmstadt.de>
2025-12-03 18:03:53 +0000

    fixed AttributeError: module 'dataland_qa_lab.review' has no attribute 'data_provider'

--- tests/review/test_yes_no_report_generator.py
+++ tests/review/test_yes_no_report_generator.py
@@ -54,9 +54,9 @@ def test_compare_yes_no_values(mock_get_yes_no_values: Mock, mock_get_yes_no_val
 
     nuclear_report = report.nuclear_energy_related_activities_section426
     fossil_report = report.fossil_gas_related_activities_section430
-    assert nuclear_report.correctedData.value == "No"
+    assert nuclear_report.corrected_data.value == "No"
     assert nuclear_report.verdict == QaReportDataPointVerdict.QAACCEPTED
-    assert fossil_report.correctedData.value is None
+    assert fossil_report.corrected_data.value is None
     assert fossil_report.verdict == QaReportDataPointVerdict.QAREJECTED
     assert fossil_report.comment == "Discrepancy in 'fossil_gas_related_activities_section430': YesNo.YES != YesNo.NO."
 
@@ -73,7 +73,7 @@ def test_build_yes_no_report_no_relevant_pages() -> None:
     assert report.fossil_gas_related_activities_section429.verdict == QaReportDataPointVerdict.QANOTATTEMPTED
 
 
-@patch("dataland_qa_lab.review.yes_no_value_generator.get_yes_no_values_from_report")
+@patch("dataland_qa_lab.review.report_generator.yes_no_report_generator.yes_no_value_generator.get_yes_no_values_from_report")
 def test_build_yes_no_report_generator_error(mock_get_yes_no_values: Mock) -> None:
     mock_get_yes_no_values.side_effect = ValueError("Error in get_yes_no_values_from_report")
 
@@ -82,10 +82,11 @@ def test_build_yes_no_report_generator_error(mock_get_yes_no_values: Mock) -> No
 
     assert report.nuclear_energy_related_activities_section426.comment == "Error in get_yes_no_values_from_report"
     assert report.nuclear_energy_related_activities_section426.verdict == QaReportDataPointVerdict.QANOTATTEMPTED
-    assert report.nuclear_energy_related_activities_section426.correctedData.value is None
+    assert report.nuclear_energy_related_activities_section426.corrected_data.value is None
 
 
-@patch("dataland_qa_lab.dataland.data_provider.get_yes_no_values_by_data")
+@patch("dataland_qa_lab.review.report_generator.yes_no_report_generator.data_provider.get_yes_no_values_by_data")
+@patch("dataland_qa_lab.review.report_generator.yes_no_report_generator.yes_no_value_generator.get_yes_no_values_from_report")
 def test_build_yes_no_report_data_provider_error(mock_get_yes_no_values_by_data: Mock, mock_get_yes_no_values_from_report: Mock) -> None:  # noqa: E501
     mock_get_yes_no_values_from_report.return_value = {}
     mock_get_yes_no_values_by_data.side_effect = ValueError("Error in get_yes_no_values_by_data")
