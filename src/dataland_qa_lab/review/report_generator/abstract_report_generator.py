fc466054cfef568054f66ccea5912452895fc7e7
Muriel926 <muriel.schindler@stud.tu-darmstadt.de>
2025-12-03 17:57:38 +0000

    abstract_report_generator.py file reformatted - blank line removed

--- src/dataland_qa_lab/review/report_generator/abstract_report_generator.py
+++ src/dataland_qa_lab/review/report_generator/abstract_report_generator.py
@@ -6,7 +6,6 @@ from azure.ai.documentintelligence.models import AnalyzeResult
 class ReportGenerator(ABC):
     """Abstract interfaces for report generators for different frameworks."""
 
-
     @abstractmethod
     def generate_report(self, relevant_pages: AnalyzeResult, dataset: any) -> any:
         """Generate a QA Report for any framework."""
