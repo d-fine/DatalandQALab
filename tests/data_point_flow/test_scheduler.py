0be0cf6f9107063163354d9f415cdd1e4a0f8762
JoshuaKoeck <joshua.koeck@stud.tu-darmstadt.de>
2026-02-04 16:31:53 +0000

    b

--- tests/data_point_flow/test_scheduler.py
+++ tests/data_point_flow/test_scheduler.py
@@ -6,6 +6,7 @@ import pytest
 from fastapi.testclient import TestClient
 
 from dataland_qa_lab.bin.server import app
+from dataland_qa_lab.data_point_flow import scheduler as data_point_scheduler
 from dataland_qa_lab.data_point_flow.scheduler import run_scheduled_processing
 from dataland_qa_lab.dataland import scheduled_processor
 
@@ -156,31 +157,14 @@ def server_mocks() -> Iterator[dict[str, Any]]:
         yield {"app": app, "config": config_mock, "scheduler": scheduler_mock, "job_func": mock_job_func}
 
 
-def test_scheduler_uses_new_logic_on_dev(server_mocks: dict[str, Any]) -> None:
-    """Test that the new scheduler logic is used in dev environment."""
+def test_scheduler_does_not_start_on_local(server_mocks: dict[str, Any]) -> None:
+    """Test that the scheduler does not add jobs in local environment."""
     mocks = server_mocks
-    mocks["config"].is_local_environment = False
-    mocks["config"].is_dev_environment = True
+    mocks["config"].is_local_environment = True
+
+    scheduler_mock = mocks["scheduler"]
+
     with TestClient(mocks["app"]):
         pass
 
-    scheduler_mock = mocks["scheduler"]
-    assert scheduler_mock.add_job.called
-
-    passed_function = scheduler_mock.add_job.call_args[0][0]
-    assert passed_function == mocks["job_func"]
-
-
-def test_scheduler_uses_old_logic_on_non_dev(server_mocks: dict[str, Any]) -> None:
-    """Test that the old scheduler logic is used in prod environment."""
-    mocks = server_mocks
-    mocks["config"].is_local_environment = False
-    mocks["config"].is_dev_environment = False
-    with TestClient(mocks["app"]):
-        pass
-
-    scheduler_mock = mocks["scheduler"]
-    assert scheduler_mock.add_job.called
-    passed_function = scheduler_mock.add_job.call_args[0][0]
-
-    assert passed_function == scheduled_processor.old_run_scheduled_processing
+    assert not scheduler_mock.add_job.called
